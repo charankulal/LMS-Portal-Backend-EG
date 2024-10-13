f5ff44995f6d38861f22ba97431725b6227f1b60
charankulal <charankulal0241@gmail.com>
2024-10-13 12:27:28 +0530

    feature: Added announcement feature for user

---
+++ LMS.api/Models/Announcement.cs
@@ -0,0 +1,9 @@
+﻿namespace LMS.api.Models
+{
+    public class Announcement
+    {
+        public int BatchId { get; set; }
+        public string Subject { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
